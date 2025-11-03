aef2cc69b824cc9ab1e6ad89939a08da80831697
Rijin Maharjan <mhzn.rijin8@gmail.com>
2025-11-03 13:01:15 +0545

    UI practice

--- app/src/main/java/com/example/functiondeclare/MainActivity.kt
+++ app/src/main/java/com/example/functiondeclare/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.functiondeclare.ui.theme.FunctionDeclareTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +28,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FunctionDeclareTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+//            Text("Hello")
+            MainBody()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainBody(){
+    //UI components
+    //@Composable required for UI function
+    Column(
+        modifier = Modifier
+//            .height(100.dp)
+//            .width(100.dp)
+//            .fillMaxHeight()
+//            .fillMaxWidth()
+            .fillMaxSize()
+            .background(color = colorResource(R.color.piggy))
+    ) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FunctionDeclareTheme {
-        Greeting("Android")
+        Text("Hello")
+        Text("World")
     }
+}
+@Preview
+@Composable
+fun PreviewBody(){
+    MainBody()
 }
